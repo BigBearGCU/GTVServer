172b3a29da3e911042d4c2e54b3f32f2b412c9b3
Brian McDonald <brian.mcdonald1980@googlemail.com>
2013-08-03 21:20:06 +0100

    Added new story

--- repository.js
+++ repository.js
@@ -16,10 +16,9 @@ exports.populateWithTestData=function()
         new testimony.TestimonyModel({name:"Dorrith Simm",pic:"Anonymous.png",
     basicInfo:"DORRITH SIM found solace in Scotland and penned book My Pocket about starting a new life in a foreign country, which is being distributed in schools across Renfrewshire to tie in with a website about the Holocaust.",
     data:"DorrithSimm.json"}),
-
-
-
-
+        new testimony.TestimonyModel({name:"Susan Singerman",pic:"Anonymous.png",
+            basicInfo:"For many years, Susan would not speak of her ordeal during the war. However, after retiring as a teacher, she began to speak to young people at schools about her experiences. She received hundreds of letters from school children saying how moved they had been by hearing her story. She felt it was vital that what had happened should not be forgotten. In 1996 she was made an MBE for services to the understanding of the Holocaust.",
+            data:"SusanSingerman.json"})
     ];
 
     testData.forEach(function(item)
