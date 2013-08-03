db4cb9fd6d2f15d46700120498065fe75e19fb16
Brian McDonald <brian.mcdonald1980@googlemail.com>
2013-08-03 22:09:35 +0100

    Added Marion Camrass

--- repository.js
+++ repository.js
@@ -18,7 +18,10 @@ exports.populateWithTestData=function()
     data:"DorrithSimm.json"}),
         new testimony.TestimonyModel({name:"Susan Singerman",pic:"Anonymous.png",
             basicInfo:"For many years, Susan would not speak of her ordeal during the war. However, after retiring as a teacher, she began to speak to young people at schools about her experiences. She received hundreds of letters from school children saying how moved they had been by hearing her story. She felt it was vital that what had happened should not be forgotten. In 1996 she was made an MBE for services to the understanding of the Holocaust.",
-            data:"SusanSingerman.json"})
+            data:"SusanSingerman.json"}),
+        new testimony.TestimonyModel({name:"Marion Camrass",pic:"Anonymous.png",
+            basicInfo:"Well I tell you 5 years ago I went to Krakow, because I wanted a copy of my birth certificate and I saw the original great big book with the names written in and everything, and everybody was very nice to me, very hospitable and very very helpful, but I did not feel at home. I had this terrible feeling, I mean the Jewish quarter of Krakow used to be teeming with life I mean teeming with life. It's empty. There is nobody there. Everybody's gone and I had the feeling all the time that if my mother hadn't taken me and run, east and away, we would both be dead and my grandchildren would not exist and I have 8 wonderful grandchildren",
+            data:"MarionCamrass.json"})
     ];
 
     testData.forEach(function(item)
