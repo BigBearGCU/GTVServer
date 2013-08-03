ff26783ed8a01e22fa54a0fc823d725b701cdc58
Brian McDonald <brian.mcdonald1980@googlemail.com>
2013-08-03 18:22:11 +0100

    updated db

--- app.js
+++ app.js
@@ -11,9 +11,27 @@ var express = require('express')
   ,mongoose = require('mongoose')
     ,repo=require('./repository');
 
-//mongodb://GTVUser:gtvpass@ds037498.mongolab.com:37498/heroku_app17298301
-var DB_URI="mongodb://GTVUser:gtvpass@ds037498.mongolab.com:37498/heroku_app17298301";
-mongoose.connect(DB_URI,'PlatformGame');
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+    process.env.MONGOLAB_URI ||
+        process.env.MONGOHQ_URL ||
+        'mongodb://localhost/HelloMongoose';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+    if (err) {
+        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + uristring);
+    }
+});
+
 var app = express();
 
 // all environments
